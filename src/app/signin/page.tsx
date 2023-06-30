92d468d025abf6b350674855694785d9ecc8fcbe
Jéssica Garcia <jessica.rodriguesgarcia@gmail.com>
2023-06-30 20:53:04 -0300

    sign-in page mobile layout

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -9,32 +9,36 @@ export default function SignInPage() {
 
   return (
     <>
-      <div className=" mx-auto my-0 flex h-screen max-w-6xl flex-col items-center justify-center gap-7 bg-gray-900 ">
-        <div className="h-35 flex w-full items-center justify-center">
-          <h1 className="mt-5 flex h-4/5 w-full items-center justify-center font-alt text-6xl uppercase tracking-widest text-green-500">
+      <div className="flex min-h-screen flex-col items-center gap-6 bg-gray-900 p-4 pt-6">
+        <div className="">
+          <h1 className="absolute right-1/2 top-[5%] flex w-full -translate-y-[5%] translate-x-1/2 justify-center  font-alt text-xl uppercase tracking-widest text-green-500">
             Diário de Músicas
           </h1>
         </div>
-        <div className="mt-8 flex h-[500px] w-[500px] flex-col rounded-full border-2 border-solid border-green-500 bg-gray-700 p-5">
-          <div className="flex h-4/5 flex-col items-center justify-between">
-            <div className="absolute right-1/2 top-1/2 h-[288px] w-[400px] -translate-y-1/4 translate-x-1/2 rounded-full bg-gray-800 blur-full" />
-            <h1 className="mt-16 flex w-4/5 items-center justify-center text-center font-text text-2xl font-bold text-green-500 drop-shadow-lg">
-              Construa o seu diário com gêneros, músicas e artistas favoritos!
+        <h1 className="border-red text-md absolute right-1/2 top-[20%] w-4/5 -translate-y-[20%] translate-x-1/2 text-center text-gray-100 drop-shadow-lg">
+          Construa o seu diário com gêneros, músicas e artistas favoritos!
+        </h1>
+        <div className="absolute right-1/2 top-1/2 mb-8 mt-6 flex h-60 w-60 -translate-y-1/2 translate-x-1/2 flex-col items-center justify-center rounded-full border-2 border-solid border-green-500 bg-gray-700 p-5">
+          <div className="flex h-4/5 w-4/5 flex-col items-center justify-between">
+            {/* <div className="absolute right-1/2 top-1/2 h-[288px] w-[400px] -translate-y-1/4 translate-x-1/2 rounded-full bg-gray-800 blur-full" /> */}
+
+            <h1 className="flex w-full justify-center text-lg font-bold text-green-500 drop-shadow-lg">
+              Aperte o Play
             </h1>
-            <div className="flex w-1/2 items-center justify-between">
+            <div className="flex w-full items-center justify-between">
               <strong>
-                <Music size={40} className="text-green-500 drop-shadow-lg" />
+                <Music className=" text-green-500 drop-shadow-lg" />
               </strong>
-              <span className="h-16 w-16 rounded-full border-2 border-solid border-green-500 bg-gray-900"></span>
+              <span className="h-14 w-14 rounded-full border-2 border-solid border-green-500 bg-gray-900"></span>
               <strong>
-                <Music size={40} className="text-green-500 drop-shadow-lg" />
+                <Music className=" text-green-500 drop-shadow-lg" />
               </strong>
             </div>
             <button
-              className="relative z-50 h-12 w-44 cursor-pointer rounded-3xl border-2 border-solid border-green-500 bg-transparent p-2 font-title text-lg font-extrabold text-green-500 drop-shadow-lg transition-colors hover:bg-green-500 hover:text-white"
+              className="text-md relative w-20 cursor-pointer rounded-3xl border-2 border-solid border-green-500 bg-transparent p-1 font-title font-extrabold text-green-500 drop-shadow-lg transition-colors hover:bg-green-500 hover:text-white"
               onClick={handleConnectSpotify}
             >
-              Fazer Login
+              Login
             </button>
           </div>
         </div>
